03f86ce379cf6308fd6fe4c141c10ae2e7dafa8c
Angel <angel92.addcs@gmail.com>
2024-02-17 18:11:19 -0500

    small ;mute fix

--- commands/mute.js
+++ commands/mute.js
@@ -3,10 +3,11 @@ const ns = require("ms");
 module.exports = {
     perms:"Mod",
     description:"Only people with the permission of [**Kick members**], this command mutes the person you mention. (Server needs an already existing 'muted' role)",
-    async execute(message, args) {
+    async execute(message, args, Discord, client) {
         if (message.member.permissions.has("KICK_MEMBERS")) {
             const target = message.mentions.users.first();
             if (!target) return message.channel.send("<@!" + message.author.id + ">" + ", This person wasnt found in this server");
+            if (target.id == client.user.id) return message.channel.send("Not enough permissions to mute this user.")
 
             let muteRole = message.guild.roles.cache.find(role => role.name == "Muted");
             let memberTarget = message.guild.members.cache.get(target.id);
